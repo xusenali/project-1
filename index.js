b4dab8145865455b56e8e88b6b223f6bb5e1ddb2
xusenali <github6121@gmail.com>
2025-04-17 12:29:28 +0300

    work

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ let base_url = 'https://67fe04863da09811b1774120.mockapi.io/users'
 
 async function loadUsers() {
     try {
-        const data = await fetch(base_url)
-        const users = await data.json()
-        console.log(users);
+        const res = await fetch(base_url)
+        const users = await res.json()
+        console.log(users)
+        creatUsers(users)
+        categoriesUsers(users)
+        categoriesCity(users)
+        if (!window.searchInitialized) {
+            initSearch(users)
+            window.searchInitialized = true
+        }
 
     } catch (error) {
-        console.log(error);
-
+        console.error("Ma'lumot yuklanmadi:", error)
     }
 }
+
 window.addEventListener('DOMContentLoaded', () => {
     loadUsers()
     getForm()
 })
-
 
 function getForm() {
     let form = document.querySelector('form')
@@ -24,15 +30,14 @@ function getForm() {
         let formData = new FormData(form)
         userspost(formData)
         form.style.display = 'none'
+
         const modalEl = document.getElementById('pupilModal');
         const modal = bootstrap.Modal.getInstance(modalEl) || new bootstrap.Modal(modalEl);
-        modal.hide();
-        
-    });
+        modal.hide()
+    })
 }
 
 function userspost(form) {
-    
     fetch(base_url, {
         method: "POST",
         headers: {
@@ -46,7 +51,99 @@ function userspost(form) {
             workPosition: form.get('work'),
             userDaraja: form.get('daraja'),
             userSalary: form.get('price'),
-            isMarried: form.has('isMared')
+            isMarried: form.has('isMarried') 
+        })
+    })
+    .then(() => {
+        loadUsers()
+    })
+    .catch(err => console.log("POST xatoligi:", err))
+}
+
+function creatUsers(users) {
+    let cont = document.querySelector('.users')
+    cont.innerHTML = ''
+    users.forEach(item => {
+        let ul = document.createElement('ul')
+        ul.style.display = "flex"
+        ul.innerHTML = `
+            <li>${item.id}</li>
+            <li>${item.name}</li>
+            <li>${item.lastName}</li>
+            <li>${item.city}</li>
+            <li>${item.date}</li>
+            <li>${item.userDaraja}</li>
+            <li>${item.workPosition}</li>
+            <li>${item.userSalary}$</li>
+            <li>${item.isMarried}</li>
+            <li>Frontend</li>
+        `
+        cont.append(ul)
+        item._element = ul
+    })
+}
+
+function initSearch(users) {
+    let input = document.querySelector('#search')
+    input.addEventListener('input', function () {
+        let searchValue = input.value.toLowerCase().trim()
+        users.forEach(user => {
+            let name = user.name.toLowerCase()
+            if (name.includes(searchValue)) {
+                user._element.style.display = "flex"
+            } else {
+                user._element.style.display = "none"
+            }
+        })
+    })
+}
+
+function categoriesUsers(allUsers) {
+    let select = document.querySelector('#filterPosition')
+    let searchInput = document.querySelector('#search')
+
+    select.addEventListener('change', () => {
+        const selectedCategory = select.value        
+        const filteredUsers = selectedCategory == 'Hammasi'
+            ? allUsers
+            : allUsers.filter(user => user.userDaraja == selectedCategory)
+        
+        creatUsers(filteredUsers)
+        
+        
+      
+        let searchValue = searchInput.value.toLowerCase().trim()
+        filteredUsers.forEach(user => {
+            if (user.name.toLowerCase().includes(searchValue)) {
+                user._element.style.display = "flex"
+            } else {
+                user._element.style.display = "none"
+            }
+        })
+    })
+}
+
+function categoriesCity(allUsers) {
+    let select = document.querySelector('#filterCity')
+    let searchInput = document.querySelector('#search')
+
+    select.addEventListener('change', () => {
+        const selectedCategory = select.value        
+        const filteredUsers = selectedCategory == 'Hammasi'
+            ? allUsers
+            : allUsers.filter(user => user.city == selectedCategory)
+        
+        creatUsers(filteredUsers)
+        
+        
+      
+        let searchValue = searchInput.value.toLowerCase().trim()
+        filteredUsers.forEach(user => {
+            if (user.name.toLowerCase().includes(searchValue)) {
+                user._element.style.display = "flex"
+            } else {
+                user._element.style.display = "none"
+            }
         })
     })
 }
